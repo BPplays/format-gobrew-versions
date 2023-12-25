b0ea32208cdfd2e509a414cd4217a797010913a1
BPplays <andymalbp@gmail.com>
2023-12-25 14:45:20 -0800

    bleh

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"regexp"
+)
+
+func gobrew_lr() (string) {
+
+
+	cmd := exec.Command("gobrew", "ls-remote")
+
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(string(output), err)
+		log.Fatal(string(output), err)
+	}
+
+	// fmt.Println(string(output))
+	return string(output)
+}
+
+
+func gobrew_parse(s string) ([]string) {
+	// Define a regular expression for splitting by comma
+	regex := regexp.MustCompile("(?m)^[A-Za-z0-9.]")
+
+	// Use the Split function to split the string
+	result := regex.Split(s, -1)
+
+	return result
+
+}
+
+
+
+
 func main() {
+
+	fmt.Println(gobrew_parse(gobrew_lr()))
 	
 }
